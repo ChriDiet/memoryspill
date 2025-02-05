2a85dad42a37c60c2b9646b1165122f60b8b461f
Ticster <empynia@gmail.com>
2025-02-05 12:19:53 +0100

    made makeCardPair() broken down task goals

--- javascript.js
+++ javascript.js
@@ -3,19 +3,33 @@
 // Kortene skal være synlige når de klikkes på, og skjules hvis de ikke matcher
 // Kortene skal inneholde bilder av (søte) dyr
 // Når alle parene er matchet skal det vises en melding om at spillet er over
+
+// 1. make function to look for clicks
+// 2. show function based on clicked square{
+//    set base img to at start.
+//    dynamicly set img to card if clicked
+// }
+// 3. hide function if img not same{
+//    dynamicly reset clicked img to base img if not equal
+//    if equal make img presistent
+// }
+// 4. keep showing img that are same
+
 document.addEventListener("DOMContentLoaded", () => {
   drawGame();
 });
 
 function drawGame() {
   document.getElementById("app").innerHTML = /*html*/ `
-        
-            <div class="card"><img src="/img/Bolla-gjeip.jpg" width="200px"/></div>
-            <div class="card"><img src="/img/Bolla-gjeip.jpg" width="200px"/></div>
-        
-        
-            <div class="card"><img src="/img/Bolla.jpg" width="200px"/></div>
-            <div class="card"><img src="/img/Bolla.jpg" width="200px"/></div>
-        
+            ${makeCardPairHtml("c1-1", "c1-2", "Bolla-gjeip")}
+            ${makeCardPairHtml("c2-1", "c2-2", "Bolla")}
+    `;
+}
+
+// all image must have .jpg extension and be in /img folder
+function makeCardPairHtml(id, id2, imgFileName) {
+  return /*html*/ `
+        <div id="${id}" class="card"><img src="/img/${imgFileName}.jpg"/></div>
+        <div id="${id2}" class="card"><img src="/img/${imgFileName}.jpg"/></div>
     `;
 }
